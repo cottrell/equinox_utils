10942060643186a71b731610de370f99f2ce5df6
David Cottrell <cottrell@users.noreply.github.com>
2023-10-23 13:06:23 +0100

    Try to fix various issues with equality testing and serialization.

--- equinox_utils/monkey_patches.py
+++ equinox_utils/monkey_patches.py
@@ -25,9 +25,10 @@ def get_summary_info(model):
 
 def monkey_patch_equinox():
     import equinox as eqx
+    from .serialization import write_equinox_via_recurse_get_state as save_model_state
 
     eqx.Module.summary_info = property(get_summary_info)
-    # eqx.Module.save_model_state = save_model_state
+    eqx.Module.save_model_state = save_model_state
 
 
 monkey_patch_equinox()
